1acec986a476f343421d03286cb59db070751282
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2024-06-04 17:27:31 +0200

    refactor(colors): reducer PoC

--- modules/color-system/global-state.ts
+++ modules/color-system/global-state.ts
@@ -1,12 +1,50 @@
 'use client'
 
+import type { Draft } from 'immer'
 import { useAtom } from 'jotai'
 import { withImmer } from 'jotai-immer'
 import { atomWithStorage } from 'jotai/utils'
 import { geistColorsConfig } from './config'
-import type { ColorSystemConfig } from './types'
+import type { ColorConfig, ColorSystemConfig } from './types'
 
 export const colorSystemAtom = withImmer<ColorSystemConfig>(atomWithStorage('pandaColorSystem_v2', geistColorsConfig))
 
 export const useColorSystem = () => useAtom(colorSystemAtom)
-const anAtom = withImmer(atomWithStorage('count', 0))
+
+type ColorSystemAction = 'add_color' | 'remove_color' | 'update_color'
+
+const colorSystemReducer = (
+  state: Draft<ColorSystemConfig>,
+  action: { type: ColorSystemAction; payload: ColorConfig },
+) => {
+  switch (action.type) {
+    case 'add_color':
+      {
+        state.colors.push(action.payload)
+      }
+      break
+    case 'remove_color':
+      {
+        state.colors = state.colors.filter((color) => color.name !== action.payload.name)
+      }
+      break
+    case 'update_color':
+      {
+        const index = state.colors.findIndex((color) => color.name === action.payload.name)
+        if (index !== -1) {
+          state.colors[index] = action.payload
+        }
+      }
+      break
+  }
+}
+
+export const useColorSystemReducer = () => {
+  const [state, setState] = useColorSystem()
+
+  const dispatch = (action: { type: ColorSystemAction; payload: ColorConfig }) => {
+    setState((draft) => colorSystemReducer(draft, action))
+  }
+
+  return [state, dispatch] as const
+}
