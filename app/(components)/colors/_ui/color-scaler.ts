087930bc0639956076ef48873f0b1e0fda1f3876
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2024-07-10 21:17:24 +0200

    refactor(colors): use culori + easing to generate color scale

--- app/(components)/colors/_ui/color-scaler.ts
+++ app/(components)/colors/_ui/color-scaler.ts
@@ -1,60 +1,11 @@
 import easing from 'bezier-easing'
-import { type Hsl, interpolate, samples } from 'culori'
+import { type Hsl, interpolate, interpolatorPiecewise, lerp, samples } from 'culori'
 import type { ColorSystemStateColorConfig } from './client-state'
 
 const bezierCurve = easing(1, 0.55, 0.6, 0.7)
 
-import { interpolatorPiecewise, lerp } from 'culori'
 function piecewiseEasing() {
   return interpolatorPiecewise((a, b, t) => lerp(a, b, bezierCurve(t)))
-}
-
-export function generateColorScale(color: ColorSystemStateColorConfig): Required<Hsl>[] {
-  const maxHueShift = color.hueShift * color.maxStops
-  const maxChromaShift = color.chromaShift * color.maxStops
-  // const luminancePerStop = (color.luminanceMax - color.luminanceMin) / color.maxStops
-  // const stopsTwoThirds = Math.floor(color.maxStops * 0.56)
-
-  const luminanceAvg = (color.luminanceMax + color.luminanceMin) / 2
-  const darkColorCount = Math.ceil(color.maxStops / 2)
-  const lightColorCount = color.maxStops - darkColorCount
-
-  const darkStartColor: Hsl = {
-    mode: 'hsl',
-    h: color.hue + maxHueShift,
-    s: (color.chroma + maxChromaShift) / 100,
-    l: color.luminanceMin / 100,
-    alpha: color.alpha / 100,
-  }
-
-  const darkEndColor: Hsl = {
-    mode: 'hsl',
-    h: color.hue,
-    s: color.chroma / 100,
-    l: color.luminanceMax / 2 / 100,
-    alpha: color.alpha / 100,
-  }
-
-  const lightStartColor: Hsl = {
-    mode: 'hsl',
-    h: color.hue,
-    s: color.chroma / 100,
-    l: color.luminanceMax / 2 / 100,
-    alpha: color.alpha / 100,
-  }
-
-  const lightEndColor: Hsl = {
-    mode: 'hsl',
-    h: color.hue,
-    s: color.chroma / 100,
-    l: color.luminanceMax / 100,
-    alpha: color.alpha / 100,
-  }
-
-  return [
-    ...generateColorScaleFrom(darkStartColor, lightEndColor, color.maxStops),
-    // ...generateColorScaleFrom(lightStartColor, lightEndColor, lightColorCount),
-  ]
 }
 
 const interpolator = {
@@ -65,6 +16,29 @@ const interpolator = {
   alpha: piecewiseEasing(),
   // biome-ignore lint/suspicious/noExplicitAny: <explanation>
 } as any
+
+export function generateColorScale(color: ColorSystemStateColorConfig): Required<Hsl>[] {
+  const maxHueShift = color.hueShift * color.maxStops
+  const maxChromaShift = color.chromaShift * color.maxStops
+
+  const startColor: Hsl = {
+    mode: 'hsl',
+    h: color.hue + maxHueShift,
+    s: (color.chroma + maxChromaShift) / 100,
+    l: color.luminanceMin / 100,
+    alpha: color.alpha / 100,
+  }
+
+  const endColor: Hsl = {
+    mode: 'hsl',
+    h: color.hue,
+    s: color.chroma / 100,
+    l: color.luminanceMax / 100,
+    alpha: color.alpha / 100,
+  }
+
+  return generateColorScaleFrom(startColor, endColor, color.maxStops)
+}
 
 function generateColorScaleFrom(startColor: Hsl, endColor: Hsl, numStops: number): Required<Hsl>[] {
   const colors = samples(numStops)
@@ -78,8 +52,6 @@ function generateColorScaleFrom(startColor: Hsl, endColor: Hsl, numStops: number
         alpha: hsl.alpha || 1,
       }
     })
-
-  // console.log({ initialColor, baseColor, endColor, colors })
 
   return colors
 }
